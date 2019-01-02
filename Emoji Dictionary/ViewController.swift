ca5adb2a2dd9aee2836ee0c193d6d7be098a352d
Luthfi Bintoro <luthfi.bintoro@websparks.sg>
2019-01-02 10:07:41 +0700

    Initial Commit

---
+++ Emoji Dictionary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Emoji Dictionary
+//
+//  Created by WS-Luthfi on 1/2/19.
+//  Copyright © 2019 interfeis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
